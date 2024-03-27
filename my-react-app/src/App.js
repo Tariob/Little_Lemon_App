eb933e2136057ea8166a41626f6c5e3ceaaa9321
Tario B <Tariob5@gmail.com>
2024-03-27 15:40:13 +0400

    new file added: header_nav_main_footer

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,18 @@
 
-import './App.css';
+import React from 'react';
+import Header from 'header';
+import Nav from  'nav';
+import Main from 'main';
+import Footer from 'footer';
 
 function App() {
   return (
-    <div>
-    HomePage
-    </div>
+    <>
+     <Header />
+     <Nav />
+     <Main />
+     <Footer />
+    </>
   );
 }
 
